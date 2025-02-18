65a116a62737e2b72ebbbd98cbe599e3a429cb35
puhalz <puhalphp@gmail.com>
2025-02-18 17:54:24 +0100

    code clean

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -1,13 +1,14 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
+// @ts-ignore
 const app = initializeApp({
-    apiKey: "AIzaSyBf60c4x1mGLg_QXhWmzGasHmCipi68Fgc",
-    authDomain: "mui-demo-app.firebaseapp.com",
-    projectId: "mui-demo-app",
-    storageBucket: "mui-demo-app.firebasestorage.app",
-    messagingSenderId: "973092051700",
-    appId: "1:973092051700:web:d8ad6c0b471822beec1ca1",
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGE_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
 });
 
 export const firebaseAuth = getAuth(app);
